ecac218e5d69d38e13161a47bd6ffbde747944c6
mina <mina@zally.com>
2026-02-23 11:36:37 +0000

    Release v1.0.0

--- Package.swift
+++ Package.swift
@@ -7,12 +7,12 @@ let package = Package(
     products: [
         .library(
             name: "zally-sdk",
-            targets: ["zally-sdk"]
+            targets: ["zally_sdk"]
         )
     ],
     targets: [
         .binaryTarget(
-            name: "zally-sdk",
+            name: "zally_sdk",
             url:
                 "https://github.com/zally-ltd/zally-ios-sdk-package/releases/download/v1.0.0/zally-sdk-1.0.0.zip",
             checksum: "f4c42efdfa7d261f11bd2e80fe49e61192c149bfa187e27ed6e03766c033746b"
